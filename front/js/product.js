f581c07ab8cdedacbe923424c8850b10fdb929f3
Shoshinailki <ptinard11@gmail.com>
2022-02-21 16:21:59 +0100

    Etape 6: insérer un produit et ses détails dans la page Produit

--- front/js/product.js
+++ front/js/product.js
@@ -6,3 +6,30 @@ if(search_params.has('id')) {
   var id = search_params.get('id');
   console.log(id);
 } 
+
+fetch(`http://localhost:3000/api/products/${id}`)
+  .then(product => product.json())
+  .then(product => {
+    console.log(product)
+    // affichage des couleurs
+    const select = document.getElementById("colors")
+    for(color of product.colors) {
+      const newoption = document.createElement("option")
+      newoption.setAttribute("value", color)
+      newoption.innerText = color
+      select.appendChild(newoption)
+    }
+    // affichage du prix
+    document.getElementById("price").innerText = product.price
+    // affichage du nom
+    document.getElementById("title").innerText = product.name
+    // affichage de la description
+    document.getElementById("description").innerText = product.description
+    // affichage de l'image
+    const divimg = document.querySelector(".item__img")
+    const img = document.createElement("img")
+    img.setAttribute("src", product.imageUrl)
+    img.setAttribute("alt", product.altTxt) 
+    divimg.appendChild(img) 
+    })
+
