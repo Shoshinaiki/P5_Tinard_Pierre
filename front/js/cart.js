e38c06f47128f3124a7e600c23e7a9f3065ded8d
Shoshinaiki <85562997+Shoshinaiki@users.noreply.github.com>
2022-03-29 17:38:03 +0200

    Etape 8: afficher un tableau recap des achats dans la page Panier 2

--- front/js/cart.js
+++ front/js/cart.js
@@ -16,18 +16,46 @@ async function displayProduct(panier){
     const parentNode = document.getElementById("cart__items"); // création du noeud parent
 
     const articleNode  = document.createElement("article"); // création article  
-    articleNode.classList.add("cart__item")
+    articleNode.classList.add("cart__item");
     articleNode.setAttribute("data-id", product.id); // attribut id
     articleNode.setAttribute("data-color", product.color); // attribut  
-    parentNode.appendChild(articleNode)
+    
 
-    const divImgNode = document.createElement("div")
-    divImgNode.classList.add("cart__item__img")
-    parentNode.appendChild(divImgNode)
+    const divImgNode = document.createElement("divImg"); // création div
+    divImgNode.classList.add("cart__item__img"); // attribut img
+    articleNode.appendChild(divImgNode)
+
     const imgNode = document.createElement("img"); // création img
     imgNode.setAttribute("src", product.info.imageUrl); // attribut src
     imgNode.setAttribute("alt", product.info.altTxt); // attribut alt
     divImgNode.appendChild(imgNode)
+
+    const divItemContentNode = document.createElement("divContent");
+    divItemContentNode.classList.add("cart__item__content");
+    
+    const divItemContentDescriptionNode = document.createElement("divDescription");
+    divItemContentDescriptionNode.classList.add("cart__item__content__description");
+    divItemContentDescriptionNode.setAttribute("Nom du produit").innerText = product.name;
+    divItemContentDescriptionNode.setAttribute("Vert", product.color);
+    divItemContentDescriptionNode.appendChild(divItemContentNode)
+
+    const divItemContentsettingsNode = document.createElement("divSettings");
+    divItemContentsettingsNode.classList.add("cart__item__content__settings");
+
+    const divItemContentQuantityNode = document.createElement("divQuantity");
+    divItemContentQuantityNode.classList.add("cart__item__content__quantity");
+    divItemContentQuantityNode.setAttribute("Qté : ", product.quantity);
+    divItemContentQuantityNode.setAttribute("number", inputType, product.info);
+    divItemContentSettingsNode.appendChild(divItemContentQuantityNode)
+
+    const divItemContentSettingsDelete = document.createElement("divDelete");
+    divItemContentSettingsDeleteNode.classList.add("cart__item__content__settings__delete");
+    divItemContentSettingsDeleteNode.setAttribute("deleteItem", product.info);
+    divItemContentsettingsNode.appendChild(divItemContentSettingsDeleteNode)
+     
+    parentNode.appendChild(articleNode)
+
+    
   }
 }
 
