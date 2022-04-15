ad839cf74914d7935718d32124e4d5e9a02b5450
Shoshinaiki <85562997+Shoshinaiki@users.noreply.github.com>
2022-04-16 00:24:37 +0200

    Afficher le numéro de commande 3

--- front/js/cart.js
+++ front/js/cart.js
@@ -167,7 +167,7 @@ function listenOrder() {
     };
     fetch("http://localhost:3000/api/products/order", options) // Envoie de la requête
       .then(function (res) {
-        console.log(orderId);
+        console.log();
         return res.json(); 
       })
       .then(function(resData) {
